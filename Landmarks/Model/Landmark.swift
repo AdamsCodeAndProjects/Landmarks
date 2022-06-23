9611bdfbbb0ae57137e8b3c4e24ecddee9bc1afe
Adam Januszewski <Adpjan@att.net>
2022-06-23 17:48:51 -0400

    Added JSON to conform with

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,40 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by adam janusewski on 6/23/22.
+//
+
+import Foundation
+import SwiftUI  // To use Hashable, Codable
+import CoreLocation  // To use CLLocationCoordinate2D
+
+//  Codable makes it easier to move data between the struct and a data file
+// Lists work on the Identifiable data.  Using Identifiable conforms it to Identifiable Protocol
+struct Landmark: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    // Reads the name of the image from the data.  The computed property will load an image from the assets
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    // These coordinates reflect the JSON data struct
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
---
+++ Landmarks/Model/ModelData.swift
@@ -0,0 +1,36 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by adam janusewski on 6/23/22.
+//
+
+import Foundation
+
+//  Array of landmarks initialized from the JSON
+var landmarks: [Landmark] = load("landmarkData.json")
+
+
+// Fetches JSON data with a given name from the app's main bundle
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't fine \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from the main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
+
